327c12fafb52f0171777d9359df8b45769672d0a
Junichi Kawahara <kawahara-j@hi-ad.jp>
2018-10-30 10:28:52 +0900

    section18

---
+++ section18/main.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        price: 19800
+    },
+    filters: {
+        localeNum: function(val) {
+            return val.toLocaleString()
+        },
+        round: function(val) {
+            return Math.round(val * 100) / 100
+        },
+        radian: function(val) {
+            return val * Math.PI / 180
+        }
+    }
+})
