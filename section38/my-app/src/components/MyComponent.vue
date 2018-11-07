8e06038bd78ee622a0ba1513c21d71da3ee96e02
Junichi Kawahara <kawahara-j@hi-ad.jp>
2018-11-07 17:47:51 +0900

    section38

---
+++ section38/my-app/src/components/MyComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+    <h2>MyComponent</h2>
+    <p>counter=<span>{{ counter }}</span></p>
+    </div>
+</template>
+<script>
+export default {
+    props: ['counter']
+}
+</script>
+<style scoped>
+span {
+    color: #f00;
+    font-weight: bold;
+}
+</style>
